69ec264cacb41c4ce007a67b6598f306d096fb7b
Florian Wiesner <wiesnerflorian94@gmail.com>
2025-11-26 10:29:14 -0500

    own riv scirpt

---
+++ eval_riv.sh
@@ -0,0 +1,120 @@
+#!/usr/bin/bash
+
+### Task name
+#SBATCH --account=sds_baek_energetic
+#SBATCH --job-name=eval_mpp
+
+### Output file
+#SBATCH --output=results/slrm_logs/eval_mpp_%j.out
+
+### Number of nodes
+#SBATCH --nodes=1
+
+### How many CPU cores to use
+#SBATCH --ntasks-per-node=18
+
+### How much memory in total (MB)
+#SBATCH --mem=300G
+
+### Mail notification configuration
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=zsa8rk@virginia.edu
+
+### Maximum runtime per task
+#SBATCH --time=24:00:00
+
+### set number of GPUs per task (v100, a100, h200)
+#SBATCH --gres=gpu:a100:1
+#SBATCH --constraint=a100_80gb
+
+
+### Partition
+#SBATCH --partition=gpu
+
+### create time series, i.e. 100 jobs one after another. Each runs for 24 hours
+##SBATCH --array=1-10%1
+
+#####################################################################################
+############################# Setup #################################################
+#####################################################################################
+
+# activate conda environment
+export CONDA_ROOT=$HOME/miniforge3
+source $CONDA_ROOT/etc/profile.d/conda.sh
+export PATH="$CONDA_ROOT/bin:$PATH"
+conda activate gphyt
+
+######################################################################################
+############################# Set paths ##############################################
+######################################################################################
+# debug mode
+# debug=true
+# Set up paths
+base_dir="/scratch/zsa8rk/multiple_physics_pretraining"
+python_bin="/home/zsa8rk/miniforge3/envs/gphyt/bin/python"
+python_exec="${base_dir}/mpp/model_eval.py"
+eval_dir="${base_dir}/results"
+
+data_dir="/scratch/zsa8rk/datasets"
+sim_name="mpp_04"
+# name of the checkpoint to use for evaluation.
+checkpoint_name="ckpt.tar_epoch20"
+# forcasts
+forecast="12 16 20 24"
+# subdir name
+sub_dir="eval/all_horizons"
+debug=false
+
+
+
+nnodes=1
+ngpus_per_node=1
+export OMP_NUM_THREADS=1
+
+
+# sim directory
+sim_dir="${eval_dir}/${sim_name}"
+
+
+
+#######################################################################################
+############################# Setup sim dir and config file ###########################
+#######################################################################################
+
+# create the sim_dir if it doesn't exist
+mkdir -p $sim_dir
+# Try to find config file in sim_dir
+config_file="${base_dir}/config/mpp_avit_b_eval.yaml"
+if [ ! -f "$config_file" ]; then
+    echo "No config_eval.yaml file found in $config_file, aborting..."
+    exit 1
+fi
+
+
+#####################################################################################
+############################# Evaluation ############################################
+#####################################################################################
+echo "--------------------------------"
+echo "Starting evaluation..."
+echo "config_file: $config_file"
+echo "sim_dir: $sim_dir"
+echo "using checkpoint: $checkpoint_name"
+echo "--------------------------------"
+
+exec_args="--config_file $config_file \
+    --sim_dir $sim_dir \
+    --data_dir $data_dir \
+    --forecast_horizons $forecast \
+    --checkpoint_name $checkpoint_name \
+    --subdir_name $sub_dir"
+
+if [ "$debug" = true ]; then
+    echo "Running in debug mode."
+    exec_args="$exec_args --debug"
+fi
+
+# Capture Python output and errors in a variable and run the script
+$python_bin $python_exec $exec_args
+
+# move the output file to the sim_dir
+mv ${sim_dir}/slrm_logs/eval_${sim_name}_${SLURM_JOB_ID}.out $sim_dir
